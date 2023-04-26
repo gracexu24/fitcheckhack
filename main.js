7e5e1c94f69d8c62008beb97a874835a8d2c23a5
ningkaiyang <ningkaiyang@gmail.com>
2023-04-25 21:25:07 -0700

    Fixed issue with ID and also variable name

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const apiKey = "600d0fd9740296eff52816c58fa120fe";
-var city =  prompt("Enter a city name:"); // Replace with the name of the city you want to get the weather for
+// const apiKey = "600d0fd9740296eff52816c58fa120fe";
+// var city =  prompt("Enter a city name:"); // Replace with the name of the city you want to get the weather for
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(`Current temperature in ${city} is: ${data.main.temp} Kelvin`);
-  })
-  .catch(error => console.error(error));
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(`Current temperature in ${city} is: ${data.main.temp} Kelvin`);
+//   })
+//   .catch(error => console.error(error));
 
 
 // Ok so first we're planning on using Google Images API to get our images, so our javascript function will return a string depending on multiple checks such as weather, 
@@ -17,7 +17,7 @@ fetch(url)
 var result; //string we will input into ai image generator
 var gender; // base this off of which gender box is checked and set to string "feminine" or "masucline"
 var aesthetic; //set to string of whichever aest was inputed 
-var location; //to store location for now
+var city; //to store location for now
 var weather; // set to "freezing" "cold" "mild" "warm" "hot" based on kelvin of place 
 //result = "Outfit for " + weather + " day with a" + gender + " " + style + " style"; 
 
@@ -26,11 +26,9 @@ var weather; // set to "freezing" "cold" "mild" "warm" "hot" based on kelvin of 
 const submitbutt = document.getElementById("subbutt");
 submitbutt.addEventListener("click", submitForm);
 
-function submitForm(event) {
-  event.preventDefault(); // Prevent default form submission behavior
-
-  var loc = document.getElementById("location");
-  location = loc.value;
+function submitForm(){
+  var loc = document.getElementById("inputBox");
+  city = loc.value;
 
   const styles = document.getElementsByName("style");
   for (let i = 0; i < styles.length; i++) {
@@ -47,6 +45,6 @@ function submitForm(event) {
     }
   }
 
-  console.log(gender + location + aesthetic + "yay it worked")
+  console.log(gender + city + aesthetic + "yay it worked");
   return false;
 }
