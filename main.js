cdbe6978d46b8c23284d4590cf5a8dbc546b734d
gracexu24 <114009551+gracexu24@users.noreply.github.com>
2023-04-26 12:20:25 -0700

    new update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// const apiKey = "600d0fd9740296eff52816c58fa120fe";
+const apiKey = "600d0fd9740296eff52816c58fa120fe";
 // var city =  prompt("Enter a city name:"); // Replace with the name of the city you want to get the weather for
 
 // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
@@ -10,15 +10,11 @@
 //   })
 //   .catch(error => console.error(error));
 
-
-// Ok so first we're planning on using Google Images API to get our images, so our javascript function will return a string depending on multiple checks such as weather, 
-// what buttons are checked in, and what outfit style they like (color? etc.)
-
 var result; //string we will input into ai image generator
 var gender; // base this off of which gender box is checked and set to string "feminine" or "masucline"
 var aesthetic; //set to string of whichever aest was inputed 
-var city; //to store location for now
-var weather; // set to "freezing" "cold" "mild" "warm" "hot" based on kelvin of place 
+var city; 
+var state; 
 //result = "Outfit for " + weather + " day with a" + gender + " " + style + " style"; 
 
 
@@ -27,8 +23,9 @@ const submitbutt = document.getElementById("subbutt");
 submitbutt.addEventListener("click", submitForm);
 
 function submitForm(){
-  var loc = document.getElementById("inputBox");
-  city = loc.value;
+  var loc = document.getElementById("inputBox").value.toString();
+  city =  loc.substring(0, str.indexOf(","));
+  state=  loc.substring(str.indexOf(",")+1);
 
   const styles = document.getElementsByName("style");
   for (let i = 0; i < styles.length; i++) {
@@ -48,3 +45,11 @@ function submitForm(){
   console.log(gender + city + aesthetic + "yay it worked");
   return false;
 }
+
+//getting data from api 
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${apiKey}`)
+   .then(response => response.json())
+   .then(data => {
+     console.log(`Current temperature in ${city}. ${state} is: ${data.main.temp} Kelvin. The weather is ${data.weather[0].description} `);
+   })
+   .catch(error => console.error(error));
