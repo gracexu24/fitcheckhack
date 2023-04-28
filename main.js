d03444e2c9b9a857496a10001f97c01556da9abf
gracexu24 <114009551+gracexu24@users.noreply.github.com>
2023-04-28 01:09:27 -0700

    we need to finish up parts but yea

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 const apiKey = "600d0fd9740296eff52816c58fa120fe";
 // var city =  prompt("Enter a city name:"); // Replace with the name of the city you want to get the weather for
 
-// const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
 // fetch(url)
 //   .then(response => response.json())
 //   .then(data => {
@@ -24,8 +22,8 @@ submitbutt.addEventListener("click", submitForm);
 
 function submitForm(){
   var loc = document.getElementById("inputBox").value.toString();
-  city =  loc.substring(0, str.indexOf(","));
-  state=  loc.substring(str.indexOf(",")+1);
+  city = loc.substring(0, loc.indexOf(","));
+  state= loc.substring(loc.indexOf(",")+1);
 
   const styles = document.getElementsByName("style");
   for (let i = 0; i < styles.length; i++) {
@@ -41,15 +39,16 @@ function submitForm(){
       break;
     }
   }
-
+  console.log(city); 
+  console.log(state); 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},US&appid=${apiKey}`;
   console.log(gender + city + aesthetic + "yay it worked");
-  return false;
+  //getting data from api 
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {console.log(`Current temperature in ${city},${state} is: ${data.main.temp} Kelvin. The weather is ${data.weather[0].description} `);
+    })
+    .catch(error => console.error(error));
+
 }
 
-//getting data from api 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${apiKey}`)
-   .then(response => response.json())
-   .then(data => {
-     console.log(`Current temperature in ${city}. ${state} is: ${data.main.temp} Kelvin. The weather is ${data.weather[0].description} `);
-   })
-   .catch(error => console.error(error));
